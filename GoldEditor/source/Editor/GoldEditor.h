bc07b454e9d5e02b1da60aa5ba7cbd283186cd29
Esteban Nova <85538374+GrstudiosGamesPro@users.noreply.github.com>
2023-08-07 02:25:14 -0500

    Camera separated

--- GoldEditor/source/Editor/GoldEditor.h
+++ GoldEditor/source/Editor/GoldEditor.h
@@ -36,6 +36,7 @@ public:
         player3->addComponent<SpriteComponent>();
 
         player3->getComponent<SpriteComponent>().cubePosition = glm::vec3(0, 999900, 0);
+
         box->start();
     }
 
@@ -46,6 +47,33 @@ public:
 
     void update() override {
         uiMaster->update();
+
+
+
+        Camera* cam = SceneManager::GetSceneManager()->OpenScene->worldCamera;
+        float cameraSpeed = 1;
+
+        if (InputSystem::GetKey(GLFW_KEY_W))
+            cam->cameraPosition += glm::vec3(0.0f, 0.0f, cameraSpeed);
+        if (InputSystem::GetKey(GLFW_KEY_S))
+            cam->cameraPosition += glm::vec3(0.0f, 0.0f, -cameraSpeed);
+        if (InputSystem::GetKey(GLFW_KEY_A))
+            cam->cameraPosition += glm::vec3(-cameraSpeed, 0.0f, 0.0f);
+        if (InputSystem::GetKey(GLFW_KEY_D))
+            cam->cameraPosition += glm::vec3(cameraSpeed, 0.0f, 0.0f);
+        if (InputSystem::GetKey(GLFW_KEY_E))
+            cam->cameraPosition += glm::vec3(0.0f, cameraSpeed, 0.0f);
+        if (InputSystem::GetKey(GLFW_KEY_Q))
+            cam->cameraPosition += glm::vec3(0.0f, -cameraSpeed, 0.0f);
+        if (InputSystem::GetKey(GLFW_KEY_UP))
+            cam->zoom -= 0.01f / 2;
+        if (InputSystem::GetKey(GLFW_KEY_DOWN))
+            cam->zoom += 0.01f / 2;
+
+        if (InputSystem::GetKey(GLFW_KEY_T)) {
+            cam->cameraPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+            cam->zoom = 1;
+        }
     }
 
     void lateupdate() override {
