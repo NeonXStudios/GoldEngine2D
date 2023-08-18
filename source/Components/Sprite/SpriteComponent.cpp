11b2828423b28d7926e5be45b7329f7c8abe6cd8
Esteban Nova <85538374+GrstudiosGamesPro@users.noreply.github.com>
2023-08-18 00:25:21 -0500

    New Render

--- source/Components/Sprite/SpriteComponent.cpp
+++ source/Components/Sprite/SpriteComponent.cpp
@@ -102,15 +102,18 @@ void SpriteComponent::start()  {
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
     // Configuraciones de la textura
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    glGenerateMipmap  (GL_TEXTURE_2D);
-    glTexParameteri   (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameterf   (GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -0.4f);
+    //glGenerateMipmap  (GL_TEXTURE_2D);
+    //glTexParameteri   (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    //glTexParameterf   (GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -0.4f);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
